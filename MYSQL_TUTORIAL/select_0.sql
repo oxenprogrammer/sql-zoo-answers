e3469d92fb1a0c7d02d50d7d5df463dad33e0d06
oxenprogrammer <emmy1000okello@gmail.com>
2021-02-09 19:07:27 +0300

    chore(SQL): add answers to select_0.sql

---
+++ MYSQL_TUTORIAL/select_0.sql
@@ -0,0 +1,59 @@
+SELECT
+  population
+FROM
+  world
+WHERE
+  name = 'Germany';
+
+SELECT
+  name,
+  population
+FROM
+  world
+WHERE
+  name IN ('Sweden', 'Norway', 'Denmark');
+
+SELECT
+  name,
+  area
+FROM
+  world
+WHERE
+  area BETWEEN 200000
+  AND 250000;
+
+SELECT
+  name,
+  population
+FROM
+  world
+WHERE
+  population BETWEEN 1000000
+  AND 1250000;
+
+SELECT
+  name
+FROM
+  world
+WHERE
+  name LIKE '%a'
+  OR name LIKE '%l';
+
+SELECT
+  name,
+  area,
+  population
+FROM
+  world
+WHERE
+  area > 50000
+  AND population < 10000000;
+
+SELECT
+  name,
+  population / area
+FROM
+  world
+WHERE
+  name IN ('China', 'Nigeria', 'France', 'Australia');
+
